34f7bb301edb7cb444825a602641b6604c1a0901
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-26 16:56:16 +0200

    Final modification where in option Sky the app shows weather symbol plus date plus hour plus temperatura at that hour.

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ import datetime
 st.title("Weather Forecast for the Next Days:")
 place = st.text_input("City:")
 
+
+# Create a slider who allow ass to select between 1 ad 5 days, sliding a bar with the mouse.
+days = st.slider(
+    "Forecast Days",
+    min_value=1,
+    max_value=5,
+    help="Select the number of forecasted days",
+)
+
+option = st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(
+    f"{option} for the next {days} days in {place}:"
+)  # create dinamyc subheader
+
+# In case there is typed an inexistent city, the program shows a friendly message instead a tecnical error:
 try:
-    # Create a slider who allow ass to select between 1 ad 5 days, sliding a bar with the mouse.
-    days = st.slider(
-        "Forecast Days",
-        min_value=1,
-        max_value=5,
-        help="Select the number of forecasted days",
-    )
-
-    option = st.selectbox("Select data to view", ("Temperature", "Sky-Date", "Sky-Temperatures"))
-    st.subheader(
-        f"{option} for the next {days} days in {place}:"
-    )  # create dinamyc subheader
-
     if place:
         # Get the temperature/sky data
         filtered_data = get_data(place, days)
@@ -38,49 +40,47 @@ try:
                 y=temperatures,
                 labels={"x": "Date", "y": "Temperature (°C)"},
             )
+
             st.plotly_chart(figure)
 
-        if option == "Sky-Date":
+        if option == "Sky":
 
+            # put the images paths in a dictionary with keywords asosiated to each weather condition:
             images = {
                 "Clear": "images/clear.png",
                 "Clouds": "images/cloud.png",
                 "Rain": "images/rain.png",
                 "Snow": "images/snow.png",
             }
+
+            # to create image paths is join the weahter condition in dictionary filtered_data with images before created and configurated:
             sky_conditions = [
                 dict["weather"][0]["main"] for dict in filtered_data
             ]
             image_paths = [images[condition] for condition in sky_conditions]
 
-            # extract the dt_txt from filtred_data and, convert it into a datatime object to give it the formatt we want.
-            day_hour = [
-                datetime.datetime.strptime(
+            # extract the dt_txt from filtred_data and, convert it into a datatime object to give it the format we want:
+            day_hours = [datetime.datetime.strptime(
                     dict["dt_txt"], "%Y-%m-%d %H:%M:%S"
                 ).strftime("%a, %d %b %H:%M")
                 for dict in filtered_data
             ]
 
-            st.image(image_paths, day_hour, width=140)
-
-        if option == "Sky-Temperatures":
-
-            images = {
-                "Clear": "images/clear.png",
-                "Clouds": "images/cloud.png",
-                "Rain": "images/rain.png",
-                "Snow": "images/snow.png",
-            }
-            sky_conditions = [
-                dict["weather"][0]["main"] for dict in filtered_data
-            ]
-            image_paths = [images[condition] for condition in sky_conditions]
-
-            # extract the dt_txt from filtred_data and, convert it into a datatime object to give it the formatt we want.
+            # extract temperatures from dict on keyword main and inside main from keyword temp, and give them the format:
             temperatures = [dict["main"]["temp"] for dict in filtered_data]
-            temperatures = [f"{int(temperature - 273)}°C" for temperature in temperatures]
+            temperatures = [
+                f"{int(temperature - 273)}°C"
+                for temperature in temperatures
+            ]
 
-            st.image(image_paths, temperatures, width=140)
+            # join strings lists day_hours and temperatures in one string list and finally
+            temp_hours = [
+                f"{day_hour} is {temperature}"
+                for day_hour, temperature in zip(day_hours, temperatures)
+            ]
+
+            # display the list temp_hours under each weather symbol:
+            st.image(image_paths, temp_hours, width=140)
 
 except KeyError:
     st.markdown(
